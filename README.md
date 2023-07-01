da26a480a3b895d7254e0f08e7d3cd16a865be0f
Dzmitry Halavach <kotik1@protonmail.com>
2023-07-01 13:27:59 +0300

    feat: more tests

--- README.md
+++ README.md
@@ -7,6 +7,6 @@
 5. Run **test scripts** in command line  `npm run test`
 6. You will see the number of skipped, passing and failing tests.
 
-I did not bother creating dev branch and pull request, all code is on master; just clone the repo and test!
+I did not bother creating dev branch and pull request, all code is on master; just clone the repo and test! 
 
 Please contact me if you have any questions at halavach@protonmail.com or using Discord
--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,52 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
+
+// import {
+//   resolveValue,
+//   throwError,
+//   throwCustomError,
+//   MyAwesomeError,
+//   rejectCustomError
+// } from '03-error-handling-async';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    const value = await resolveValue('value');
+    expect(value).toBe('value');
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    expect(() => {
+      throwError('test error message');
+    }).toThrow('test error message');
   });
+});
 
-  test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+describe('throwError', () => {
+  test('should throw error with default message if no message is provided', () => {
+    expect(() => {
+      throwError();
+    }).toThrow('Oops!');
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    expect(() => {
+      throwCustomError();
+    }).toThrow(MyAwesomeError);
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    await expect(rejectCustomError()).rejects.toThrow(MyAwesomeError);
   });
 });
