27187859e3a3aa72b261ab7c624976564fa5f60d
Dzmitry Halavach <kotik1@protonmail.com>
2023-07-02 16:34:01 +0300

    feat: snapshot tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,21 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const mockLinkedList = generateLinkedList([1]);
+    expect(mockLinkedList).toStrictEqual({
+      next: { next: null, value: null },
+      value: 1,
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const mockLinkedList = generateLinkedList([1, 2]);
+    expect(mockLinkedList).toMatchSnapshot({
+      next: { next: { next: null, value: null }, value: 2 },
+      value: 1,
+    });
   });
 });
